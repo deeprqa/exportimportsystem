96a86fefbcd019a24db09370a538c1004e3f2538
Deep Rathod <deeprqa@gmail.com>
2021-11-13 16:51:51 +0530

    This is the first 3 source code files implementing expoert system

---
+++ Test2.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+describe('My first Test Suite', function(){
+
+    it('My First Test Case Function', function(){
+      
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca')
+        cy.get('div.product').should('have.length', 4);
+    })
+})
